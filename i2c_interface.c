ddf80f3f71dff1706bec955d5ade206552660554
Tyler Barkin <tybarkin@gmail.com>
2019-07-07 23:56:18 +0000

    The scheme is working. Build up the rest of the functions and once that is working we can start moving in the bulk of the driver

--- i2c_interface.c
+++ i2c_interface.c
@@ -4,26 +4,26 @@
 
 #define PCF8591_I2C_ADDR 0x68
 
-void i2c_write_test(void){
+int i2c_write_test(void){
 
 	printf("Testing I2C");
 
-	// int handle;
-	// unsigned char command[2];
-	// unsigned char value[4];
+	int handle;
+	unsigned char command[2];
+	unsigned char value[4];
 
-	// command[0] = 0x40;
-	// command[1] = 0x20;
+	command[0] = 0x40;
+	command[1] = 0x20;
 
-	// if (gpioInitialise() < 0) return 1;
-	// handle = i2cOpen(1, PCF8591_I2C_ADDR, 0);
+	if (gpioInitialise() < 0) return 1;
+	handle = i2cOpen(1, PCF8591_I2C_ADDR, 0);
 
-	// i2cWriteDevice(handle, command, 2);
+	i2cWriteDevice(handle, command, 2);
 
-	// value[0] = i2cReadByte(handle);
-	// printf("%3d", value[0]);
+	value[0] = i2cReadByte(handle);
+	printf("%3d", value[0]);
 
-	// i2cClose(handle);
-	// gpioTerminate();
+	i2cClose(handle);
+	gpioTerminate();
 
 }
--- i2c_interface.h
+++ i2c_interface.h
@@ -1,7 +1,7 @@
 #ifndef _I2C_INTERFACE_H_
 #define _I2C_INTERFACE_H_
 
-	void i2c_write_test(void);
+	int i2c_write_test(void);
 
 #endif  /* #ifndef _I2C_INTERFACE_H_ */
 
