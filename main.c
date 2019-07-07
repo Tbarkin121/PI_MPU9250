90842a196172ff6f31b2a4063f0fa5cd480671ee
Tyler Barkin <tybarkin@gmail.com>
2019-07-07 19:00:40 -0400

    Building up slowly, making sure things will compile right before digging in

---
+++ main.c
@@ -0,0 +1,22 @@
+/**
+ *   @defgroup  eMPL
+ *   @brief     Embedded Motion Processing Library
+ *
+ *   @{
+ *       @file      main.c
+ *       @brief     Test app for eMPL using the Motion Driver DMP image.
+ */
+ 
+/* Includes ------------------------------------------------------------------*/
+#include "stdio.h"
+
+#include "main.h"
+    
+#include "inv_mpu.h"
+
+
+int main(void)
+{ 
+  
+ 
+}
