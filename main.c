437beef758393fd669788b58e56afb2f40c7c4eb
Tyler Barkin <tybarkin@gmail.com>
2019-07-09 01:08:23 -0400

    Trying to get thing initalized

--- main.c
+++ main.c
@@ -12,6 +12,38 @@
 
 #include "main.h"
 #include "inv_mpu.h"
+#include "mltypes.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Data read from MPL. */
+#define PRINT_ACCEL     (0x01)
+#define PRINT_GYRO      (0x02)
+#define PRINT_QUAT      (0x04)
+#define PRINT_COMPASS   (0x08)
+#define PRINT_EULER     (0x10)
+#define PRINT_ROT_MAT   (0x20)
+#define PRINT_HEADING   (0x40)
+#define PRINT_PEDO      (0x80)
+#define PRINT_LINEAR_ACCEL (0x100)
+#define PRINT_GRAVITY_VECTOR (0x200)
+
+volatile uint32_t hal_timestamp = 0;
+#define ACCEL_ON        (0x01)
+#define GYRO_ON         (0x02)
+#define COMPASS_ON      (0x04)
+
+#define MOTION          (0)
+#define NO_MOTION       (1)
+
+/* Starting sampling rate. */
+#define DEFAULT_MPU_HZ  (20)
+
+#define FLASH_SIZE      (512)
+#define FLASH_MEM_START ((void*)0x1800)
+
+#define PEDO_READ_MS    (1000)
+#define TEMP_READ_MS    (500)
+#define COMPASS_READ_MS (100)
 
 void test_fun(void) {
     printf("test 1 \n");
