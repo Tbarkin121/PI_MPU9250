3fd809e625dd21b5da36bf8944ea62ed370cc7b9
Tyler Barkin <tybarkin@gmail.com>
2019-07-14 04:37:55 -0400

    Added a test folder. In this I call a C function from a C++ program. This can be used as a template for the ROS Node

---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+extern "C" {
+	#include "sub.h"
+}
+
+int main(int argc, char *argv[])
+{
+	std::cout<<"Test c++"<<std::endl;
+	test_fun();
+}
---
+++ test/sub.c
@@ -0,0 +1,6 @@
+#include "stdio.h"
+#include "sub.h"
+
+void test_fun(void){
+	printf("Test Fun c \n");
+}
